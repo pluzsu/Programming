85002a405a8cc6562eb3876922c8078c46d5dc79
Pluhár Zsuzsa <pluharzs@ludens.elte.hu>
2024-09-10 23:21:43 +0200

    Update and rename Siknegyed2.cs to siknegyed2.cs

--- 2gyakorlat/siknegyed2.cs
+++ 2gyakorlat/siknegyed2.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 
-public class SiknegyedRekord
-{
-struct Pont
-    {
+public class SiknegyedRekord {
+struct Pont {
         public double x;
         public double y;
     };
     //static void Main(string[] args)
-    public SiknegyedRekord()
-    {
+    public SiknegyedRekord() {
         Pont p;
         int q = 0;
 
@@ -18,20 +15,16 @@ struct Pont
         Console.Write("y: ");
         double.TryParse(Console.ReadLine(), out p.y); 
         
-        if (p.x >= 0 && p.y >= 0)
-        {
+        if (p.x >= 0 && p.y >= 0) {
             q = 1;
         }
-        else if (p.x < 0 && p.y >= 0)
-        {
+        else if (p.x < 0 && p.y >= 0) {
             q = 2;
         }
-        else if (p.x < 0 && p.y < 0)
-        {
+        else if (p.x < 0 && p.y < 0) {
             q = 3;
         }
-        else if (p.x >= 0 && p.y < 0)
-        {
+        else if (p.x >= 0 && p.y < 0) {
             q = 4;
         }
 
