7b1ea2e41a4ce5d354e0a388c96184940e1daa0e
Pluhár Zsuzsa <pluharzs@ludens.elte.hu>
2024-09-10 23:14:38 +0200

    Update Faktorialis.cs

--- 2gyakorlat/Faktorialis.cs
+++ 2gyakorlat/Faktorialis.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-public class Faktorialis
-{
-    //static void Main(string[] args)
-    public Faktorialis()
-	{
+﻿using System; 
+public class Faktorialis {
+  static void Main(string[] args) {
+    public Faktorialis() {
         int x;
         int i = 1;
         int fakt = 1;
@@ -12,18 +9,15 @@ public class Faktorialis
         Console.Write("x: ");
         int.TryParse(Console.ReadLine(), out x);
 
-        if (x < 0 || x > 14)
-        {
+        if (x < 0 || x > 14) {
             Console.WriteLine("0 <= n <= 14");
-            return;
-        }
-
-        while (i <= x)
-        {
-            fakt = fakt * i;
-            i = i + 1;
-        }
+        } else {
+            while (i <= x) {
+              fakt = fakt * i;
+              i = i + 1;
+           }
         
         Console.WriteLine("{0} faktoriális = {1}", x, fakt);
-    }
-}
+       }
+   }
+ }
